748489e8bbae006b9b808ca8e17ca8cdb911a79f
Sanketh Mopuru <sanketh.mopuru@gmail.com>
2014-03-20 20:10:53 +0530

    Parse Table generation completed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <set>
 
 using namespace std;
 #define EPSILON -1
@@ -16,16 +17,16 @@ public:
 		len_terminals = lt;
 	}
 
-	vector<vector<int> > first(){
+	vector<set<int> > first(){
 		int lnt = rules.size();
-		vector<vector<int> > first_set( len_terminals + lnt );
-		vector<int> temp;
+		vector<set<int> > first_set( len_terminals + lnt );
+		set<int> temp;
 		int i,j;
 
 		/*Terminal symbols can produce only themselves*/
 		/*So, first sets of terminal symbols are trivial*/
 		for(i = lnt; i < len_terminals+lnt;i++){
-			temp.push_back(i);
+			temp.insert(i);
 			first_set[i]=temp;
 			temp.clear();
 		}
@@ -34,13 +35,6 @@ public:
 		/*Algo to compute the first sets of non-terminals*/
 
 
-		/*Print the first_sets*/
-		for(i=0;i<first_set.size();i++){
-			for(j=0;j<first_set[i].size();j++){
-				cout << first_set[i][j] << " ";
- 			}
- 			cout << endl;
-		}
 
 
 		//Return the first set
@@ -53,24 +47,43 @@ public:
 	}
 };
 
-bool in_vector(vector<int> set, int val){
-	int i;
-	for(i=0;i<set.size();i++){
-		if(set[i] == val){
-			return true;
+set<int> get_first( vector<set<int> > first_sets,  vector<int> symbols  ){
+	set<int> first_set;
+	int i,j;
+	set<int>::iterator it;
+	bool is_in;
+
+	for(i=0; i < symbols.size() ; i++  ){
+		if(symbols[i] == EPSILON)
+			continue;
+
+		for(it = first_sets[symbols[i]].begin() ; it != first_sets[symbols[i]].end() ; ++it  ){
+			first_set.insert(*it);
+		}
+		is_in = (bool)(first_sets[symbols[symbols[i]]].find(EPSILON) != first_sets[symbols[i]].end());
+		if(!is_in){
+			break;
 		}
 	}
-	return false;
+
+	if(first_set.empty()){
+		first_set.insert(EPSILON);
+	}
+
+	return first_set;
 }
+
 
 /*Computes and returns the parse table*/
 vector<vector<vector<int> > > 
-compute_parse_table(vector<vector<int> > first_sets, vector<vector<int> > follow_sets, Rules rules ){
+compute_parse_table(vector<set<int> > first_sets, vector<set<int> > follow_sets, Rules rules ){
 	int lnt = rules.rules.size();
 	int lt = rules.len_terminals;
 	vector<vector<vector<int> > > parse_table(lnt);
 	vector<vector<int> > row(lt);
 	int i,j,k;
+	set<int> temp;
+	set<int>::iterator it;
 
 	for(i=0;i<lnt;i++){
 		parse_table[i] = row;
@@ -88,16 +101,21 @@ compute_parse_table(vector<vector<int> > first_sets, vector<vector<int> > follow
 
 	for(i=0;i<lnt;i++){
 		for(j=0;j<rules.rules[i].size();j++){
+			temp = get_first(first_sets, rules.rules[i][j]);
 			//For each production
-			//For each terminal in first_sets(production)
+			//For each terminal in first_sets(RHS of production)
 			//code here
+			for(it = temp.begin() ; it != temp.end() ; ++it){
+				if ( *it == EPSILON )
+					continue;
+				parse_table[i][*it-lnt] = rules.rules[i][j];
+			}
 
-
-			//If EPSILON is in first_sets[i]
+			//If EPSILON is in first_sets(RHS of production)
 			//for each terminal including $ in follow_sets[i]
-			if(in_vector( first_sets[i], EPSILON ) ){
-				for(k=0;k<follow_sets[i].size();k++){
-					parse_table[i][follow_sets[i][k]-lnt] = rules.rules[i][j];
+			if( (bool)( temp.find(EPSILON) != temp.end() ) ){
+				for(it = follow_sets[i].begin() ; it != follow_sets[i].end();  ++it){
+					parse_table[i][*it - lnt] = rules.rules[i][j];
 				}
 			}
 		}
@@ -111,6 +129,7 @@ compute_parse_table(vector<vector<int> > first_sets, vector<vector<int> > follow
 
 
 }
+
 
 
 int main(){
